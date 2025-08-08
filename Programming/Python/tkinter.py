af8cea8e3ee4bdbc6ca054ffb134001d396e139f
learninglizard1 <SachinSinghBade@myop.ac.nz>
2025-08-08 14:14:37 +1200

    Add files via upload

---
+++ Programming/Python/tkinter.py
@@ -0,0 +1,10 @@
+#import tkinter
+
+import tkinter as tk
+from tkinter import messagebox
+
+messagebox.showinfo ("Your message box title", "This is the messagebox text!")
+
+window = tk.Tk()
+window.title("Your title here")
+window.mainloop()
