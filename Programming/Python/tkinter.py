6a8e01bf45e3ac0b7fdef3d0b4d7ee27d0218ed3
learninglizard1 <SachinSinghBade@myop.ac.nz>
2025-08-08 15:46:42 +1200

    Update tkinter.py

--- Programming/Python/tkinter.py
+++ Programming/Python/tkinter.py
@@ -14,7 +14,7 @@ window.mainloop()
 
 
 
-#creating a labels like a telephone number
+#Using grid to make a label look like a telephone number
 #import tkinter
 
 import tkinter as tk
@@ -45,3 +45,58 @@ label8.grid (row = 2, column = 1) #position at cell 2,1
 label9.grid (row = 2, column = 2) #position at cell 2,2
 
 root.mainloop()
+
+
+
+#using place to make a face and greeting message
+import tkinter as tk 
+root = tk.Tk() 
+root.title("Place example")  
+ 
+
+left_eye = tk.Label(root, text = "O") 
+right_eye = tk.Label(root, text = "O")
+mouth = tk.Label(root, text = "____")
+
+
+message = tk.Label(root, text = "Greetings!") 
+
+ 
+# Position left eye 
+left_eye.place(x = 0, y = 20 ) 
+# Position right eye 
+right_eye.place(x = 40, y = 20) 
+# Position mouth 
+mouth.place(x = 15, y = 24) 
+# Position message 
+message.place(x = 0, y = 60) 
+
+
+
+root.mainloop()
+
+
+
+#using pack to place the buttons
+
+import tkinter as tk 
+root = tk.Tk() 
+root.title("Pack example") 
+
+button1 = tk.Button(text = "*****") 
+button2 = tk.Button(text = "**") 
+button3 = tk.Button(text = "*") 
+button4 = tk.Button(text = "****") 
+button5 = tk.Button(text = "***") 
+
+# Add your pack statements here
+button3.pack ()
+button2.pack ()
+button5.pack ()
+button4.pack ()
+button1.pack ()
+ 
+root.mainloop()
+
+
+
