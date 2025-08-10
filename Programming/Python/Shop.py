915e23c6118cd77a792239ade30ed189b9e4476c
learninglizard1 <SachinSinghBade@myop.ac.nz>
2025-08-10 23:24:03 +1200

    Update Shop.py

--- Programming/Python/Shop.py
+++ Programming/Python/Shop.py
@@ -101,35 +101,3 @@ button.pack()
 root.mainloop()
 
 
-
-#using global * to import tkinter so we dont need to call in every variable or function.
-from tkinter import *
-from tkinter import messagebox
-
-
-def ButtonEvent():
-    messagebox.showinfo ("Alert notice", "This is a message box")
-
-def ExitApp():
-    exit()
-
-
-root = Tk()
-root.title("Events demo")
-
-text_label = Label(root, text="This is text").grid (row= 0, column= 0)
-
-button_go = Button(root, text="Go", width= 15, command = ButtonEvent)
-button_go.grid (row=1 , column= 0)
-
-button_next = Button(root, text= "Next", width = 15, command= ButtonEvent)
-button_next.grid (row=1, column=1)
-
-button_before = Button(root, text= "Before", width= 15, command= ButtonEvent)
-button_before.grid (row=1, column=2)
-
-button_exit = Button(root, text= "Exit", width= 15, command= ExitApp)
-button_exit.grid (row=1, column=3)
-
-
-root.mainloop()
