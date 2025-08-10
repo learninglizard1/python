da44bcbe846b27cd2e8681322d9d2a8bc055be83
learninglizard1 <SachinSinghBade@myop.ac.nz>
2025-08-10 23:24:31 +1200

    Update tkinter.py

--- Programming/Python/tkinter.py
+++ Programming/Python/tkinter.py
@@ -265,3 +265,83 @@ button.grid (row= 4, column= 0 , padx =  5, pady = 5)
 
 
 root.mainloop()
+
+
+
+
+#using global * to import tkinter so we dont need to call in every variable or function.
+from tkinter import *
+from tkinter import messagebox
+
+
+def ButtonEvent():
+    messagebox.showinfo ("Alert notice", "This is a message box")
+
+def ExitApp():
+    exit()
+
+
+root = Tk()
+root.title("Events demo")
+
+text_label = Label(root, text="This is text").grid (row= 0, column= 0)
+
+button_go = Button(root, text="Go", width= 15, command = ButtonEvent)
+button_go.grid (row=1 , column= 0)
+
+button_next = Button(root, text= "Next", width = 15, command= ButtonEvent)
+button_next.grid (row=1, column=1)
+
+button_before = Button(root, text= "Before", width= 15, command= ButtonEvent)
+button_before.grid (row=1, column=2)
+
+button_exit = Button(root, text= "Exit", width= 15, command= ExitApp)
+button_exit.grid (row=1, column=3)
+
+
+root.mainloop()
+
+
+
+#temperature conversion
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+root.title("Temperature Convertor")
+
+def button_clicked():
+    num = convert.get()
+    if (num == ""):
+        messagebox.showerror("Error", "No value entered.")
+        return
+    
+    if (not num.isdigit()):
+        messagebox.showerror("Error", "Invalid characters")
+        return
+    
+    result = f_to_c(int(num))
+
+    new_text = f"%sF = %ic"%(num , result)
+    lbl_display.config (text = new_text)
+
+
+def f_to_c(f):
+    return (f - 32) * (5/9)
+
+convert = StringVar()
+
+lbl_fahrenheit =Label(root, text= "Fahrenheit").grid(row= 0, column= 0)
+entry= Entry( root, textvariable= convert).grid (row= 0, column= 1)
+button = Button (root, text= "Convert", command= button_clicked).grid (row= 1, column= 0, columnspan= 2)
+lbl_display = Label (root, text= "")
+lbl_display.grid (row= 2, column= 0, columnspan= 2)
+
+
+
+
+root.mainloop()
+
+
+
+
