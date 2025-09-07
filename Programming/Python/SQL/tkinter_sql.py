d2d3758d41ed302a5c29e59970fa19fd8fcfc12b
learninglizard1 <SachinSinghBade@myop.ac.nz>
2025-09-08 10:33:49 +1200

    Update tkinter_sql.py

--- Programming/Python/SQL/tkinter_sql.py
+++ Programming/Python/SQL/tkinter_sql.py
@@ -1,67 +1,77 @@
+# Import modules
 import sqlite3
 from sqlite3 import Error
 import tkinter as tk
 from tkinter import ttk
 
-
+# Tkinter setup
 root = tk.Tk()
-root.geometry("650x250")
+root.geometry("600x250")
 
-
+# The path/name of our database file
+# It is convenient to keep it somewhere easy to see and edit
 database_path = "fruit.db"
 
+####################
+# Database functions
 
-def dbconnect():  #The dbconnect() is almost the same as before but we don’t use a global scope variable anymore, we just return the database object.
+# Connects to the database, returns the database object
+#The dbconnect() is almost the same as before but we don’t use a global scope variable anymore, we just return the database object.
 
+def dbconnect():
     conn = None
     try:
         conn = sqlite3.connect(database_path)
     except Error as e:
-        print (e)
+        print(e)
         return None
     return conn
 
-def execute_query(query): #The execute_query() function is intended for our insert, update, and delete interactions. It has the commit() function that these require.
-    #connect to the database
+# Executes a query
+# We can use this for insert, update, and delete
+# It will not work for select since we need to get the rows
+#The execute_query() function is intended for our insert, update, and delete interactions. It has the commit() function that these require.
+def execute_query(query):
+    # Connect to the database
     conn = dbconnect()
-    #get the cursor object
+    # Get the cursor object
     cursor = conn.cursor()
-    #run the query
+    # Run the query
     cursor.execute(query)
-    #commit changes
+    # Commit changes
     conn.commit()
-    #close database
-    conn.close()
+    # Close database
+    cursor.close()
 
-def select_query(query): #the select_query() is used when we want to get data. It will return the rows when we provide a query.
-    #connect to the database
+#the select_query() is used when we want to get data. It will return the rows when we provide a query.
+def select_query(query):
+     # Connect to the database
     conn = dbconnect()
-    #get the cursor object
+    # Get the cursor object
     cursor = conn.cursor()
-    #run the query
+    # Run the query
     cursor.execute(query)
-    rows= cursor.fetchall()
-    #close databse
-    conn.close()
-    #return the rows we obtained
+    rows = cursor.fetchall()
+    # Close database
+    cursor.close()
+    # Return the rows we obtained
     return rows
 
-
-
+# Database functions END
+########################
 
 rows = select_query("SELECT * FROM Fruit;")
-
-#print the rows we found
-final_text=""
-rows_found= len(rows)
+# Print the rows we found
+final_text = ""
+rows_found = len(rows)
 for row in rows:
     final_text += f"{row[0]} \t {row[1]} | {row[2]} | {row[3]} \n"
 
-
-lbl_count = ttk.Label(root, text= f"Total rows: {rows_found}")
+lbl_count = ttk.Label(root, text=f"Total rows: {rows_found}")
 lbl_rows = ttk.Label(root, text=final_text)
 
-lbl_count.grid(row=0, column=0)
-lbl_rows.grid(row=1, column=0)
+lbl_count.grid(row = 0, column = 0)
+lbl_rows.grid(row = 1, column = 0)
 
+# Tkinter loop
 root.mainloop()
